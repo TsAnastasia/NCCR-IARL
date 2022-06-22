a21107394f3322b1da897f38320cc79967c59106
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-06-22 15:19:08 +0300

    fix styles

--- src/components/pageTitle/PageTitle.tsx
+++ src/components/pageTitle/PageTitle.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
 import scss from "./pageTitle.module.scss";
 
-const PageTitle: FC<{ name: string }> = ({ name }) => {
-  return <h1 className={scss.title}>{name}</h1>;
+const PageTitle: FC<{ name: string; className?: string }> = ({
+  name,
+  className,
+}) => {
+  return <h1 className={`${className} ${scss.title}`}>{name}</h1>;
 };
 
 export default PageTitle;
