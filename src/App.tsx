14209bd047b225dac4e7bafd2a1b46c50e3d1765
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-07-28 14:25:53 +0300

    update webpack

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import AppRouter from "./router/AppRouter";
 import scss from "./app.module.scss";
 import { useMatch } from "react-router-dom";
 import { RouteNames } from "./router/router";
-import "@fontsource/roboto";
 
 const App = () => {
   const isStructurePage = useMatch(RouteNames.STRUCTURE);
